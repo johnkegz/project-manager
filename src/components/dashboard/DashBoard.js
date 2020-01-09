117c6aa7ba52756ef1222c6ad2945f7a58ead544
johnkegz <johnkal24@gmail.com>
2020-01-09 12:48:16 +0300

    Syncing Data with Firestore

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react'
-import Notification from './Notification';
-import ProjectList from '../project/ProjectList';
-import { connect } from 'react-redux';
- 
-export class DashBoard extends Component {
+import React, { Component } from "react";
+import Notification from "./Notification";
+import ProjectList from "../project/ProjectList";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
-    
-    render() {
-        const { projects } = this.props;
-        return (
-            <div className="dashboard container">
-                <div className="row">
-                    <div className="col s12 m6">
-                        <ProjectList projects={projects} />
-                    </div>
-                    <div className="col s12 m5 offset-m1">
-                        <Notification />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+export class DashBoard extends Component {
+  render() {
+    const { projects } = this.props;
+    return (
+      <div className='dashboard container'>
+        <div className='row'>
+          <div className='col s12 m6'>
+            <ProjectList projects={projects} />
+          </div>
+          <div className='col s12 m5 offset-m1'>
+            <Notification />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        projects: state.projects.projects
-    }
-}
+  return {
+    // projects: state.projects.projects
+    projects: state.firestore.ordered.projects
+  };
+};
 
-export default connect(mapStateToProps)(DashBoard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    {
+      collection: "projects"
+    }
+  ])
+)(DashBoard);
