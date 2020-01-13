3ac17a2a40471a8fe8de80db7ec9233c3ef633e4
johnkegz <johnkal24@gmail.com>
2020-01-13 14:13:01 +0300

    auth trigger

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,6 @@ const createNotification = (notification => {
   return admin.firestore().collection("notifications")
     .add(notification)
     .then(doc => {
-        console.log("here >>");
         return console.log("Notification added", doc)
     });
 });
@@ -22,7 +21,6 @@ const createNotification = (notification => {
 exports.projectCreated = functions.firestore
   .document("projects/{projectId}")
   .onCreate(doc => {
-      console.log(" +++++++++", doc._fieldsProto);
     const project = doc._fieldsProto;
     const notification = {
       content: "Add a new project",
@@ -31,3 +29,16 @@ exports.projectCreated = functions.firestore
     };
     return createNotification(notification);
   });
+
+  exports.userJoined = functions.auth.user().onCreate(user => {
+      return admin.firestore().collection("users").doc(user.uid).get().then(doc => {
+          const newUser = doc.data();
+          console.log("newUser.firstName ++++", newUser.firstName);
+          const notification = {
+              content: "Joined the party",
+              user: `${newUser.firstName} ${newUser.lastName}`,
+              time: admin.firestore.FieldValue.serverTimestamp()
+          }
+          return createNotification(notification);
+      })
+  })
