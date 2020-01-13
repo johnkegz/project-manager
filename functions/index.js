6babdce6c6669e23ed299357b395a299bd397fdc
johnkegz <johnkal24@gmail.com>
2020-01-13 13:13:29 +0300

    Add create project notification trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("yoo");
+  response.send("yoo");
 });
+
+const createNotification = (notification => {
+  return admin.firestore().collection("notifications")
+    .add(notification)
+    .then(doc => {
+        console.log("here >>");
+        return console.log("Notification added", doc)
+    });
+});
+
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+      console.log(" +++++++++", doc._fieldsProto);
+    const project = doc._fieldsProto;
+    const notification = {
+      content: "Add a new project",
+      user: `${project.authorFirstName.stringValue} ${project.authorLastName.stringValue}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+    return createNotification(notification);
+  });
--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react'
 export default class Notification extends Component {
     render() {
         return (
-            <div>
-                <p>Notification</p>
+            <div className="section">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">Notifications</span>
+                        <ul className="notifications">
+                            <li>Notification</li>
+                            <li>Notification</li>
+                            <li>Notification</li>
+                            <li>Notification</li>
+                            <li>Notification</li>
+                            <li>Notification</li>
+                            <li>Notification</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
